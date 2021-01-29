2f20b887fe8b1704e5dad3988c7bda5a1abf2616
Victor <35394796+victor-myha@users.noreply.github.com>
2021-01-29 23:02:34 +0200

    Заливаю сайт портфоліо (не стилізований ще)

---
+++ github projects/My Portfolio/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.multiple-items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        infinity: true,
+        dots:false,
+        centerMode: true,
+        centerPadding:'0',
+      });
+}); 
